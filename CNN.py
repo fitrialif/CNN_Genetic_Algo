fa917f5a7b9b69d1e8f1d06b20eab4091b67ac0f
johncookds <johncookds@gmail.com>
2017-10-05 09:05:19 -0400

    CNN

---
+++ CNN.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+import numpy as np
+import scipy as sp
+import sklearn.ensemble as ske
+from sklearn.cross_validation import cross_val_score
+import tensorflow as tf
+from __future__ import print_function
+import inspect
+
+class cnn(object):
+
+	def __init__(self):
+		pass
+
+
+	def set_structure(self,n_inputs,hidden_lvls,n_classes):
+		
+
+
